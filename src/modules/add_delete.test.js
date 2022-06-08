b6755d5506778dda6bab57098eab4f9af6ba2ce6
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-06-08 17:09:08 +0100

    Adding changes to the test

--- src/modules/add_delete.test.js
+++ src/modules/add_delete.test.js
@@ -1,22 +1,24 @@
-const {addFunction} = require('./add.js');
-const {deleting} = require('./delete.js');
+const { addFunction } = require("./add.js");
+const { deleting } = require("./delete.js");
 
-
-describe('deleting', () => {
-  it('Should delete object array', () => {
-    const i = 0
-    let toDoArray = [{description: 'test1', completed: false, index: 0}]
+// Addin
+describe("deleting", () => {
+  // deleting the object
+  it("Should delete object array", () => {
+    const i = 0;
+    let toDoArray = [{ description: "test1", completed: false, index: 0 }];
     const result = [];
     expect(deleting(i, toDoArray)).toEqual(result);
-  })
-  it('Should add object array', () => {
+  });
+  // checking whether the object is an array
+  it("add object to create an  array", () => {
     let arr = [];
     const objTemplate = {
-      description: 'test1',
+      description: "test1",
       completed: false,
       index: 0,
     };
-    const result = [{description: 'test1', completed: false, index: 0}];
+    const result = [{ description: "test1", completed: false, index: 0 }];
     expect(addFunction(arr, objTemplate)).toEqual(result);
-  })
-})
+  });
+});
